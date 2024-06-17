6e5f5f9db9854df91d75de01a905bfde67ee956d
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-06-17 18:54:06 +0300

    Add task 2.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+const onTimeUpdate = throttle(({ seconds }) => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
+}, 1000);
+
+player.on('timeupdate', onTimeUpdate);
+
+const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+if (savedTime) {
+  player.setCurrentTime(savedTime).catch(error => {
+    console.error('Error setting current time:', error);
+  });
+}
